15159d6bb2bfeeca30596e8101a0ddf631f2c4d0
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-08-29 09:50:55 +0300

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -1,14 +1,17 @@
 #pragma once
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <stdint.h>
 #include <string>
-
+#include <vector>
+#include "enet/include/enet.h"
+#include "GameUpdatePacket.h"
+#include "proton/variant.hpp"
 
 namespace Utils {
 	std::vector<std::string> Split(const std::string &str,const std::string &delimiter);
 	std::string Random(int len);
 	std::string GenerateMac();
+	int GetMessageType(ENetPacket* packet)
+	gameupdatepacket_t* GetStruct(ENetPacket* packet);
 	bool Replace(std::string& str, const std::string& from, const std::string& to);
 	bool HandleDelay(uint64_t& alpha, int beta);
 	uint64_t GetTime();
