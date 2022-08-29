5439085d8b9f15d32e62116e8d5e8a9a4d8dfa19
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-08-29 09:53:46 +0300

    Update Utils.cpp

--- Utils.cpp
+++ Utils.cpp
@@ -1,13 +1,10 @@
-
-
 #include "Utils.h"
-
+#include <algorithm>
 #include <chrono>
-#include <tchar.h>
+#include <random>
+#include <regex>
 
-// Utils
-
-std::vector<std::string> Utils::Split(const std::string &str,const std::string &delimiter) {
+std::vector<std::string> Utils::Split(const std::string& str, const std::string& delimiter) {
 	std::vector<std::string> arr;
 	int strleng = str.length();
 	int delleng = delimiter.length();
@@ -16,58 +13,41 @@ std::vector<std::string> Utils::Split(const std::string &str,const std::string &
 	int k = 0;
 	while (i < strleng) {
 		int j = 0;
-		while (i + j < strleng && j < delleng && str[i+j] == delimiter[j]) j++;
+		while (i + j < strleng && j < delleng && str[i + j] == delimiter[j]) j++;
 		if (j == delleng) {
-			arr.push_back(str.substr(k,i-k));
+			arr.push_back(str.substr(k, i - k));
 			i += delleng;
 			k = i;
-		} else i++;
+		}
+		else i++;
 	}
 	arr.push_back(str.substr(k, i - k));
 	return arr;
 }
 
-std::string random_str = "0123456789ABCDEF";
-
-std::string Utils::Random(int len) {
-	std::string m_ret = "";
-	for (int i = 0; i < len; i++)
-		m_ret += random_str[rand() % random_str.length()];
-	return m_ret;
+int Utils::GetMessageType(ENetPacket* packet) {
+     int result;
+     if (packet->dataLength > 3u) {
+        result = *(packet->data);
+     } else {
+	cout << "Bad packet length, ignoring message" << endl;
+       	result = 0;
+     }
+     return result;
 }
 
-std::string Utils::GenerateMac() {
-	std::string m_ret = "";
-	for (int i = 0; i < 6; i++) {
-		if (i > 0)
-			m_ret += ":";
-		m_ret += random_str[rand() % random_str.length()];
-		m_ret += random_str[rand() % random_str.length()];
-	}
-	return m_ret;
+gameupdatepacket_t* Utils::GetStruct(ENetPacket* packet) {
+    if (packet->dataLength < sizeof(gameupdatepacket_t) - 4)
+        return nullptr;
+    gametankpacket_t* tank = reinterpret_cast<gametankpacket_t*>(packet->data);
+    gameupdatepacket_t* gamepacket = reinterpret_cast<gameupdatepacket_t*>(packet->data + 4);
+    if (gamepacket->m_packet_flags & 8) {
+        if (packet->dataLength < gamepacket->m_data_size + 60) {
+            printf("got invalid packet. (too small)\n");
+            return nullptr;
+        }
+        return reinterpret_cast<gameupdatepacket_t*>(&tank->m_data);
+    } else
+        gamepacket->m_data_size = 0;
+    return gamepacket;
 }
-
-bool Utils::Replace(std::string& str, const std::string& from, const std::string& to) {
-	size_t start_pos = str.find(from);
-	if (start_pos == std::string::npos)
-		return false;
-	str.replace(start_pos, from.length(), to);
-	return true;
-}
-
-bool Utils::HandleDelay(uint64_t& alpha, int beta) {
-	if (alpha < GetTime()) {
-		alpha = GetTime() + beta;
-		return true;
-	} else
-		return false;
-}
-
-uint64_t Utils::GetTime() {
-	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-}
-
-bool Utils::is_number(const std::string& s) {
-	return !s.empty() && std::find_if(s.begin() + (*s.data() == '-' ? 1 : 0), s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
-}
-
