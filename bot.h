ed24f977a4751ba39041194a58450491787b7c6a
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-08-29 09:45:46 +0300

    Update bot.h

--- bot.h
+++ bot.h
@@ -14,4 +14,28 @@ class GrowtopiaBot {
 public:
   	ENetPeer *peer;
   	ENetHost *client;
+    World *world;
+    
+    void ClientHost();
+    void Init();
+	  void Loop();
+  
+    void OnConnected();
+	  void OnDisconnected();
+    
+    void OnLoginRequest();
+    void OnGenericText(ENetPacket *packet);
+    void OnGameMessage(std::string text);
+	  void packet_type6(std::string text);
+	  void ProcessPacket(ENetEvent* event, ENetPeer* peer);
+	
+	  void OnPlayerState(gameupdatepacket_t* packet);
+	  void OnPacketCallFunction(gameupdatepacket_t* packet);
+  	void OnTileChangeRequest(gameupdatepacket_t* packet);
+  	void OnSendMapData(gameupdatepacket_t* packet);
+  	void OnSendTileUpdateData(gameupdatepacket_t* packet);
+  	void OnSendInventoryState(gameupdatepacket_t* packet);
+  	void OnSendTreeState(gameupdatepacket_t* packet);
+  	void OnModifyItemInventory(gameupdatepacket_t* packet);
+  	void OnItemChangeObject(gameupdatepacket_t* packet);
 }
