a583bd5236bff4498b8c1d731665b1e70cd25c6e
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-08-29 09:16:26 +0300

    Update bot.h

--- bot.h
+++ bot.h
@@ -1,1 +1,17 @@
-//
+#pragma once
+#include "enet/include/enet.h"
+#include <string>
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <time.h>
+#include "proton/variant.hpp"
+#include "GameUpdatePacket.h"
+#include "World/ObjectData.h"
+
+class GrowtopiaBot {
+public:
+  	ENetPeer *peer;
+  	ENetHost *client;
+}
