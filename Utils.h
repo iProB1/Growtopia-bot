bd410a4e25757f89444467c3a128577717f207b1
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-08-29 09:54:36 +0300

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -8,12 +8,6 @@
 
 namespace Utils {
 	std::vector<std::string> Split(const std::string &str,const std::string &delimiter);
-	std::string Random(int len);
-	std::string GenerateMac();
 	int GetMessageType(ENetPacket* packet)
 	gameupdatepacket_t* GetStruct(ENetPacket* packet);
-	bool Replace(std::string& str, const std::string& from, const std::string& to);
-	bool HandleDelay(uint64_t& alpha, int beta);
-	uint64_t GetTime();
-	bool is_number(const std::string& s);
 };
