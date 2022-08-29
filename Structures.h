5e7f23e7d18b88d575ae67220f29af954bd27e64
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-08-29 09:23:47 +0300

    Create Structures.h

---
+++ Structures.h
@@ -0,0 +1,20 @@
+struct Players
+	{
+		std::string name;
+	  std::string country;
+	  int NetID = -1;
+	  int UserID = -1;
+	  int x = -1;
+	  int y = -1;
+	  bool gone = false;
+	  bool local = false;
+	  bool mod = false;
+	};
+
+class World {
+public:
+     std::string name;
+     int Width = 0;
+     int Height = 0;
+     std::vector<Players> players;
+}
